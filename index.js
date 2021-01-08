d4fe62a121069aa5d88a50774ab10f4f430911a8
TheProgrammer21 <lukaswindisch21@gmail.com>
2021-01-08 01:02:10 +0100

    Added functionality

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
+const axios = require("axios");
 const Redis = require("ioredis");
 const redis = new Redis();
 
 loadData();
 
-async function loadData(){
-    let data = (await axios.get('http://www.statistik.at/verzeichnis/strassenliste/gemplzstr_6.csv')).data;
+async function loadData() {
+    let data = (await axios.get("http://www.statistik.at/verzeichnis/strassenliste/gemplzstr_6.csv")).data;
 
     data = data.split("\n");
     data.shift();
@@ -14,5 +14,32 @@ async function loadData(){
     data.pop();
     data.pop();
 
-    redis.set("foo", "bar");
+    for (let dataset of data) {
+        dataset = dataset.split(";");
+        // Gemeindekennziffer;Politische Gemeinde;Ortschaftskennziffer;Ortschaftsname;Straßenkennziffer;Straßenname;Postleitzahl;Gemeindecode
+
+        let address = {
+            gemeindekennziffer: dataset[0],
+            politischeGemeinde: dataset[1],
+            ortschaftskennziffer: dataset[2],
+            ortschaftsname: dataset[3],
+            strassenKennziffer: dataset[4],
+            strassenName: dataset[5],
+            postleitzahl: dataset[6],
+            gemeindecode: dataset[7]
+        };
+
+        let x = await redis.hset(
+            `strasse:${address.ortschaftskennziffer}:{${address.strassenKennziffer}}:${address.postleitzahl}`,
+            "gemeindekennziffer", address.gemeindekennziffer,
+            "politische_gemeinde", address.politischeGemeinde,
+            "ortschaftskennziffer", address.ortschaftskennziffer,
+            "ortschaftsname", address.ortschaftsname,
+            "strassenkennziffer", address.strassenKennziffer,
+            "strassenname", address.strassenName,
+            "postleitzahl", address.postleitzahl
+        );
+
+        console.log(x);
+    }
 }
